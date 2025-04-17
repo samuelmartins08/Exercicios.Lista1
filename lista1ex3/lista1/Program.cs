c077565d534855fe88db789471378ff104d5f01c
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-04-16 22:24:35 -0300

    Add files via upload

---
+++ lista1ex3/lista1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double diagonal;
+            double area;
+
+            Console.Write("Informe o valor da diagonal: ");
+            diagonal = double.Parse(Console.ReadLine());
+           
+
+            area = (diagonal * diagonal) / 2;
+
+            Console.WriteLine("O valor da area é: {0}", area);
+
+
+             
+
+
+            
+        }
+    }
+}
