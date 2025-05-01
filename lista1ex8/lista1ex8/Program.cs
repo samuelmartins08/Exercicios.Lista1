c79c7e8117fefa27a944f0a4dd30cec218c17b10
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-05-01 17:54:00 -0300

    Add files via upload

---
+++ lista1ex8/lista1ex8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor em Celsius: ");
+            double cels = double.Parse(Console.ReadLine());
+
+            double mult = cels * 1.8;
+            double result = mult + 32;
+
+            Console.WriteLine("O valor em fahrenheit é: {0}", result);
+
+
+        }
+    }
+}
