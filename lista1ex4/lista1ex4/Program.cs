af3d496757c9b112b43833ad735d579842c08ec6
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-04-30 21:56:19 -0300

    Add files via upload

---
+++ lista1ex4/lista1ex4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baze;
+            double alt;
+            double area;
+
+
+            Console.WriteLine("Digite o valor da base: ");
+            baze = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor da altura: ");
+            alt = double.Parse(Console.ReadLine());
+
+            area = baze * alt;
+
+            Console.WriteLine("Calculando a area do triangulo utilizando a altura e base, temos: {0} ", area);
+
+
+
+
+        }
+    }
+}
---
+++ lista1ex5/lista1ex5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double val1;
+            double val2;
+            double val3;
+            double val4;
+            double soma;
+            double result;
+
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            val1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            val2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            val3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            val4 = double.Parse(Console.ReadLine());
+
+            soma = val1 + val2 + val3 + val4;
+            result = soma / 4;
+
+            Console.WriteLine("O valor da media aritimética é: {0}", result);
+
+
+        }
+    }
+}
