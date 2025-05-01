0d3e9defe11c3bd77dbcf0025e727123cdba25f2
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-04-30 22:06:55 -0300

    Add files via upload

---
+++ lista1ex7/lista1ex7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("Digite a quantidade de milhas marítimas: ");
+                double milhasMaritimas = double.Parse(Console.ReadLine());
+
+                double metros = milhasMaritimas * 1852;
+                double quilometros = metros / 1000;
+
+                Console.WriteLine($"{milhasMaritimas} milhas marítimas equivalem a {quilometros:F3} km.");
+            }
+        }
+    }
+}
+
+
