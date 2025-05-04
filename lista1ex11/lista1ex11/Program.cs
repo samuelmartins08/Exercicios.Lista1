f421ebd8fbe4785f233c6b1cdf4096f7943bf745
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-05-04 11:38:01 -0300

    Add files via upload

---
+++ lista1ex11/lista1ex11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor de X: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valore de Y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            double elev = Math.Pow(x, y);
+
+            Console.WriteLine("O resultado de {0} elevado a {1} é: {2}", x, y, elev);
+        }
+    }
+}
