a4de02fdd4b8429097214ec7fc5142044cc618b5
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-05-04 11:12:57 -0300

    Add files via upload

---
+++ lista1ex9/lista1ex9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do diametro: ");
+            double diam = double.Parse(Console.ReadLine());
+            
+            double pi = 3.14159;
+
+            double diam2 = diam / 2;
+            double area = pi * diam2 * diam2;
+
+            Console.WriteLine("A area calculada é: {0}", area);
+           
+        }
+    }
+}
